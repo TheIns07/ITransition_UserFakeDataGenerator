4a9d7d7ac791399cf10e3d898e822b787f5b9237
TheIns07 <arturoinscreup2401@hotmail.com>
2024-07-27 17:53:43 -0700

    Add project files.

---
+++ UserDataGenerator.Server/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace UserDataGenerator.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly Faker _faker;
+        
+        public UserController()
+        {
+            _faker = new Faker();
+        }
+
+        [HttpGet("{region}/{errors}/{seed}")]
+        public IActionResult GetUsers(string region, int errors, int seed)
+        {
+            _faker.Locale = region.ToLower() switch
+            {
+                "poland" => "pl",
+                "usa" => "en",
+                "georgia" => "ka",
+                _ => "en"
+            };
+
+            _faker.Random = new Randomizer(seed);
+
+            var users = new List<UserDTO>();
+            for (int i = 0; i < 20; i++)
+            {
+                var user = new UserDTO
+                {
+                    Id = i + 1,
+                    Identifier = Guid.NewGuid(),
+                    Name = _faker.Name.FullName(),
+                    Address = _faker.Address.FullAddress(),
+                    Phone = _faker.Phone.PhoneNumber()
+                };
+
+                ApplyErrors(user, errors);
+                users.Add(user);
+            }
+
+            return Ok(users);
+        }
+
+        private void ApplyErrors(UserDTO user, int errors)
+        {
+            var errorCount = (int)Math.Floor(errors * 0.1 * 5);
+            for (int i = 0; i < errorCount; i++)
+            {
+                var randomField = _faker.PickRandom("Name", "Address", "Phone");
+                switch (randomField)
+                {
+                    case "Name":
+                        user.Name = _faker.Random.String2(10);
+                        break;
+                    case "Address":
+                        user.Address = _faker.Random.String2(20);
+                        break;
+                    case "Phone":
+                        user.Phone = _faker.Random.String2(10);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ UserDataGenerator.Server/Models/FakeUserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace UserDataGenerator.Server.Models
+{
+    public class FakeUserDTO
+    {
+        public int Id { get; set; }
+        public Guid Identifier { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
